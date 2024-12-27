9ee1bbf9ded505bf83b17285ea1437d1dffb04b7
Ashtaa <ashenafisahele@gmail.com>
2024-12-27 13:48:45 +0300

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Replace '/weather/' with your GitHub repo name
 export default defineConfig({
+  base: '/weather/', 
   plugins: [react()],
-})
+});
